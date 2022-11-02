3c0065543f057d3faa01917b0bf57efef84eb5f5
NgocNguyen <nguyenminhngoc531@gmail.com>
2022-11-02 13:14:13 +0000

    Them helloworld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,38 @@
-print('Hello world')
+# print('Hello world')
+
+#001
+#firstname = input ("Nhap vao ten: ")
+#print ('Hello', firstname) 
+
+#002: 
+# firstname = input ("Nhap vao ten: ")
+# surname = input ("Nhap vao Ho: ")
+# print ('Hello', firstname, surname)
+
+#003
+# print ('what do you call a bear with no teeth? \n gummy bear')
+
+#004
+# num1 = int (input("Please enter your first number: "))
+# num2 = int(input("Please enter your second number: "))
+# answer = num1 + num2
+# print("The answer is", answer)
+
+#005
+# num1 = int(input("Please enter your first numder: "))
+# num2 = int(input("Please enter your second number: "))
+# num3 = int(input("Please enter your third number: "))
+# answer = num1 + num2 + num3
+# print("The answer is", answer)
+
+#006
+# startNum = int(input("Enter the number of slices of pizza you started with: "))
+# endNum = int(input("How many slices have you eaten? "))
+# slicesLeft = startNum - endNum
+# print ("You have", slicesLeft, "slices remaining")
+
+#007
+name = input ("What is your name? ")
+age = int(input("How old are you?"))
+newAge = age + 1
+print(name,"next birthday you will be", newAge)
